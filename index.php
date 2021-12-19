c4c893ca36fdc4a468affe98a65af3211589689d
rbfreires <rbfreires@gmail.com>
2021-12-19 16:53:39 -0300

    criando form

--- index.php
+++ index.php
@@ -13,16 +13,55 @@
 
 <body>
 
+    <!-- heading -->
+    <nav class="navbar navbar-dark" style="background-color: goldenrod;">
+        <div class="container-fluid"><a href="" class="navbar-brand"><img src="" alt="" class="d-line-block align-text-top">
+                Olá! Benvindo ao painel de Login!
+            </a>
+        </div>
+    </nav>
 
+    <!-- starting the form -->
+    <div class="container">
+        <form action="" class="row g-3">
+            <div class="col-sm-4">
+                <label for="" class="form-label">Nome</label>
+                <input type="text" name="nome" id="nome" class="form-control form-control-sm">
+            </div>
+            <div class="col-sm-5">
+                <label for="" class="form-label">Endereço</label>
+                <input type="text" name="endereco" id="endereco" class="form-control form-control-sm">
+            </div>
+            <div class="col-sm-3">
+                <label for="" class="form-label">Bairro</label>
+                <input type="text" name="bairro" id="bairro" class="form-control form-control-sm">
+            </div>
+            <div class="col-sm-3">
+                <label for="" class="form-label">Cidade</label>
+                <input type="text" name="cidade" id="cidade" class="form-control form-control-sm">
+            </div>
+            <div class="col-sm-2">
+                <label for="" class="form-label">UF</label>
+                <input type="text" name="uf" id="uf" class="form-control form-control-sm">
+            </div>
+            <div class="col-sm-3">
+                <label for="" class="form-label">CEP</label>
+                <input type="text" name="cep" id="cep" class="form-control form-control-sm">
+            </div>
+        </form>
+    </div>
 
 
 
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
